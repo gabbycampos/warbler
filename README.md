d0b9ac230afcc3da8419c9bd8d168ac1fec383ff
Gabriela Campos <gabriela@Gabrielas-MacBook-Pro-2.local>
2021-02-15 10:45:56 -0600

    added testing

---
+++ README.md
@@ -0,0 +1,11 @@
+# A twitter clone 
+
+> This application is a clone of the twitter app. Users can make an account, post messages, like messages, follow other users. As well as unlike, unfollow, and delete their own messages.
+
+#### Technologies used in the project:
+- Flask
+- WTForms
+- PostgresSQL
+- unittest
+
+![alt text](static/images/app.png)
--- models.py
+++ models.py
@@ -33,17 +33,17 @@ class User(db.Model):
 
     __tablename__ = 'users'
 
-    id = db.Column(db.Integer,primary_key=True,)
-    email = db.Column(db.Text,nullable=False,unique=True,)
-    username = db.Column(db.Text,nullable=False,unique=True,)
-    image_url = db.Column(db.Text, default="/static/images/default-pic.png",)
-    header_image_url = db.Column(db.Text, default="/static/images/warbler-hero.jpg",)
-    bio = db.Column(db.Text,)
-    location = db.Column(db.Text,)
-    password = db.Column(db.Text,nullable=False,)
-    messages = db.relationship('Message',)
-    followers = db.relationship("User",secondary="follows",primaryjoin=(Follows.user_being_followed_id == id),secondaryjoin=(Follows.user_following_id == id),)
-    following = db.relationship("User",secondary="follows",primaryjoin=(Follows.user_following_id == id),secondaryjoin=(Follows.user_being_followed_id == id),)
+    id = db.Column(db.Integer,primary_key=True)
+    email = db.Column(db.Text,nullable=False,unique=True)
+    username = db.Column(db.Text,nullable=False,unique=True)
+    image_url = db.Column(db.Text, default="/static/images/default-pic.png")
+    header_image_url = db.Column(db.Text, default="/static/images/warbler-hero.jpg")
+    bio = db.Column(db.Text)
+    location = db.Column(db.Text)
+    password = db.Column(db.Text,nullable=False)
+    messages = db.relationship('Message')
+    followers = db.relationship("User",secondary="follows",primaryjoin=(Follows.user_being_followed_id == id),secondaryjoin=(Follows.user_following_id == id))
+    following = db.relationship("User",secondary="follows",primaryjoin=(Follows.user_following_id == id),secondaryjoin=(Follows.user_being_followed_id == id))
     likes = db.relationship('Message',secondary="likes")
 
     def __repr__(self):
@@ -109,10 +109,10 @@ class Message(db.Model):
 
     __tablename__ = 'messages'
 
-    id = db.Column(db.Integer,primary_key=True,)
-    text = db.Column(db.String(140),nullable=False,)
-    timestamp = db.Column(db.DateTime,nullable=False,default=datetime.utcnow(),)
-    user_id = db.Column(db.Integer,db.ForeignKey('users.id', ondelete='CASCADE'),nullable=False,)
+    id = db.Column(db.Integer,primary_key=True)
+    text = db.Column(db.String(140),nullable=False)
+    timestamp = db.Column(db.DateTime,nullable=False,default=datetime.utcnow())
+    user_id = db.Column(db.Integer,db.ForeignKey('users.id', ondelete='CASCADE'),nullable=False)
     user = db.relationship('User')
 
 
